6f9d301ada24dd0c73d05dabed06f6852cf73f22
dmoranj <dmoranj@gmail.com>
2018-03-12 08:53:20 +0100

    ADD First version of generator

--- src/wordpredict.py
+++ src/wordpredict.py
@@ -8,7 +8,7 @@ def extract_model_features(model):
     regex = re.compile(r"\.layers\.recurrent\.(.*)'", re.IGNORECASE)
     rnnLayers = [str(type(layer)) for layer in model.layers if str(type(layer)).find('layers.recurrent') > 0]
     cellType = [regex.findall(layer)[0] for layer in rnnLayers][0]
-    _, sequenceSize = model.input
+    _, sequenceSize = model.input.shape
 
     return len(rnnLayers), cellType, sequenceSize
 
@@ -29,20 +29,49 @@ def load_vocabulary(path):
         return vocabulary
 
 
-def generate(model, vocabulary, sequenceSize, options):
+def reverse_vocabulary(vocabulary):
+    return {value: key for key, value in vocabulary.items()}
+
+
+def generate(model, sequenceSize):
     print('generating')
 
+    input = np.zeros((1, sequenceSize))
+    output_words = []
+
+    for i in range(1, sequenceSize -1):
+        output = model.predict(input)
+
+        last_word = int(np.round(output[0, i - 1]*1500))
+
+        if last_word <= 0:
+            break
+
+        input[0, i] = last_word
+        output_words.append(last_word)
+
+    return output_words
 
 
+def print_full_line():
+    full_line = '\n' + 50*'-' + '\n\n'
+    print(full_line)
 
 
-def display_result():
-    print('displaying')
+def display_result(vocabulary, final_output):
+    reversed_vocabulary = reverse_vocabulary(vocabulary)
 
+    words = [reversed_vocabulary[word_index] for word_index in final_output]
+    text = ' '.join(words)
+
+    print('\n\nThe generated text is:\n\n')
+    print_full_line()
+    print(text)
+    print_full_line()
 
 def generation(options):
     print('Generating...')
     vocabulary = load_vocabulary(options.vocabularyPath)
     model, _, _, sequenceSize =load_model(options.modelPath)
-    final_output, stats = generate(model, vocabulary, sequenceSize, options)
-    display_result(vocabulary, final_output, stats)
+    final_output = generate(model, sequenceSize)
+    display_result(vocabulary, final_output)
